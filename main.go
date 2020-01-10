f8c4fe247b397288f7392b1cc1794c2f6e93e341
pqguanyinli <34409013+pqguanyinli@users.noreply.github.com>
2020-01-10 19:56:52 +0800

    Update main.go

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 
 	o := opts.New(&s)
 	o.Version(VERSION)
-	o.Repo("https://github.com/pqguanyinli/simple-torrent")
+	o.Repo("https://github.com/pqguanyinli/cloud-torrent")
 	o.PkgRepo()
 	o.SetLineWidth(96)
 	o.Parse()
